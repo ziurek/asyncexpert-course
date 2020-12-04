83f6b9e88de4bcdb3e0f8901d7fa1fd6635c857c
ziurek <pawmazur92@gmail.com>
2020-12-04 17:12:33 +0100

    Module 04 homework await false, and TaskCompletionSource

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -5,13 +5,32 @@ namespace AwaitableExercises.Core
 {
     public static class BoolExtensions
     {
+        public static BoolAwaiter GetAwaiter(this bool boolValue)
+        {
+            return new BoolAwaiter(boolValue);
+        }
     }
 
     public class BoolAwaiter : INotifyCompletion
     {
+        private readonly bool _value;
+
+        public BoolAwaiter(bool value)
+        {
+            _value = value;
+        }
+
+        public bool IsCompleted => false;
+
+        public bool GetResult()
+        {
+            return _value;
+        }
+
         public void OnCompleted(Action continuation)
         {
-            throw new NotImplementedException();
+            continuation();
+            return;
         }
     }
 }
--- Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
+++ Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
@@ -8,7 +8,32 @@ namespace TaskCompletionSourceExercises.Core
     {
         public static Task<string> RunProgramAsync(string path, string args = "")
         {
-            return Task.FromResult(string.Empty);
+            var taskCompletionSource = new TaskCompletionSource<string>();
+
+            var process = new Process();
+            process.EnableRaisingEvents = true;
+            process.StartInfo = new ProcessStartInfo(path)
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                Arguments = args
+            };
+            process.Exited += (sender, eventArgs) =>
+            {
+                var senderProcess = sender as Process;
+                if (senderProcess?.ExitCode == 0)
+                {
+                    taskCompletionSource.TrySetResult(senderProcess?.StandardOutput.ReadToEnd());
+                }
+                else
+                {
+                    taskCompletionSource.TrySetException(new Exception(senderProcess?.StandardError.ReadToEnd()));
+                }
+                senderProcess?.Dispose();
+            };
+            process.Start();
+
+            return taskCompletionSource.Task;
         }
     }
 }
